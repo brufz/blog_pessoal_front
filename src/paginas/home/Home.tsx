ec415b6317621a9e73d5cd4f8fb301c4bfc37139
Bruna Franchi <bfzdev@gmail.com>
2022-07-26 16:07:28 -0300

    iniciando projeto blogpessoal frontend com react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Home.css';
+
+function Home() {
+  return (
+    <>
+      <h1 className='titulo'>Home</h1>
+      <img src="https://www.stoodi.com.br/wp-content/uploads/2018/11/como-se-escreve-por-do-sol.jpg" alt="imagem por do sol" className='img'/>
+    </>
+  )
+}
+export default Home
