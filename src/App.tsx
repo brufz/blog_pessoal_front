0644a0bbd7c650fc2cc6e2504cc9694a7b0af3e0
Bruna Franchi <bfzdev@gmail.com>
2022-07-27 20:13:49 -0300

    adicionando navbar, home e footer do blog pessoal

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import Navbar from './components/static/navbar/Navbar';
+import Footer from './components/static/footer/Footer'; 
 import Home from './paginas/home/Home'
 import './App.css';
 
 function App() {
   return (
+   <>
+   <Navbar />
    <Home />
+   <Footer />
+      
+   </>
   );
 }
 
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from 'react';
+import {Typography, Box, Grid, Button} from '@material-ui/core';
 import './Home.css';
 
 function Home() {
   return (
     <>
-      <h1 className='titulo'>Home</h1>
-      <img src="https://www.stoodi.com.br/wp-content/uploads/2018/11/como-se-escreve-por-do-sol.jpg" alt="imagem por do sol" className='img'/>
+       <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
+      
     </>
   )
 }
